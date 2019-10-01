eba87b64324059b437361694d14294f9516961ef
Sam <samroman3@gmail.com>
2019-10-01 15:59:28 -0400

    Adds ui element outlets, adds imagepicker etension and corresponding methods

--- imagePickerLab/imagePickerLab/ViewController.swift
+++ imagePickerLab/imagePickerLab/ViewController.swift
@@ -10,11 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var folderbutton: UIButton!
+    
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    
+    @IBAction func cameraAction(_ sender: UIButton) {
+        imagePickerViewcontroller.sourceType = .camera
+        present(imagePickerViewcontroller, animated: true)
+    }
+    
+    
+    @IBAction func folderAction(_ sender: UIButton) {
+        imagePickerViewcontroller.sourceType = .photoLibrary
+        present(imagePickerViewcontroller, animated: true, completion: nil)
+    }
+    
+    
+    var photoLibraryAccess = false
+    
+    private var imagePickerViewcontroller: UIImagePickerController!
+    
+    
+    
+    private func setupImagePickerViewController() {
+               imagePickerViewcontroller = UIImagePickerController()
+               imagePickerViewcontroller.delegate = self
+               if !UIImagePickerController.isSourceTypeAvailable(.camera) {
+                   cameraButton.isEnabled = false
+               }
+       }
+    
     override func viewDidLoad() {
+        setupImagePickerViewController()
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+        } else {
+            print("original image is nil")
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
 
